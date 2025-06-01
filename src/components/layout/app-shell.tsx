628d49349260c821b4ae332934a6a14d654131c2
Will rock <willrockoscar@gmail.com>
2025-06-01 17:59:42 +0000

    still laggy to move between the tabs, remove hover on the overview tabs

--- src/components/layout/app-shell.tsx
+++ src/components/layout/app-shell.tsx
@@ -4,16 +4,21 @@
 import React from 'react';
 import { Sidebar, SidebarInset, SidebarRail } from '@/components/ui/sidebar';
 import { AppSidebar } from './app-sidebar';
+import { usePathname } from 'next/navigation';
 
 export function AppShell({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname();
   return (
     <div className="flex min-h-screen">
       <Sidebar collapsible="icon" variant="sidebar" side="left">
         <AppSidebar />
         <SidebarRail />
       </Sidebar>
-      <SidebarInset className="flex-1">
-        <div className="p-4 sm:p-6 lg:p-8">
+      <SidebarInset className="flex-1 bg-transparent">
+        <div 
+          key={pathname} /* Key change triggers re-render and animation */
+          className="p-4 sm:p-6 lg:p-8 animate-content-fade-in"
+        >
          <div className="mx-auto max-w-7xl">
             {children}
           </div>
